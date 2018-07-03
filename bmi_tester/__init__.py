a4e546e6032f772e76ed455fc98dc1489edfc5bb
mcflugen <mcflugen@gmail.com>
2018-07-03 16:39:25 -0600

    Set up versioneer for version management.

--- bmi_tester/__init__.py
+++ bmi_tester/__init__.py
@@ -3,3 +3,7 @@ from .api import run_test
 
 
 __all__ = ['BmiTester', 'run_test']
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #! /usr/bin/env python
 from setuptools import setup, find_packages
 
+import versioneer
+
 
 setup(name='bmi-tester',
-      version='0.1.0',
+      version=versioneer.get_version(),
       author='Eric Hutton',
       author_email='eric.hutton@colorado.edu',
       description='Test Python BMI bindings.',
       long_description=open('README.md').read(),
       packages=find_packages(),
+      cmdclass=versioneer.get_cmdclass(),
       entry_points={
           'console_scripts': [
               # 'bmi-tester=bmi_tester.cmd:main',
