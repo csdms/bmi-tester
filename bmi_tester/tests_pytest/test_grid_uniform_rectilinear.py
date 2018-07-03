ff460f9a3c7df3322271eeb5de3bead72fe121bc
mcflugen <mcflugen@gmail.com>
2018-07-03 16:35:55 -0600

    Remove test for get_grid_shape for scalar grids.

--- bmi_tester/tests_pytest/test_grid_uniform_rectilinear.py
+++ bmi_tester/tests_pytest/test_grid_uniform_rectilinear.py
@@ -22,19 +22,6 @@ def test_get_grid_shape(new_bmi, gid):
         for dim in shape:
             assert dim > 0
 
-    if gtype == 'scalar':
-        ndim = new_bmi.get_grid_rank(gid)
-
-        shape = np.empty(ndim, dtype=np.int32)
-        try:
-            rtn = new_bmi.get_grid_shape(gid, shape)
-        except TypeError:
-            warnings.warn('get_grid_shape should take two arguments')
-            rtn = new_bmi.get_grid_shape(gid)
-        else:
-            assert rtn is shape
-        np.testing.assert_equal(shape, ())
-
 
 def test_get_grid_spacing(new_bmi, gid):
     """Test the grid spacing."""
