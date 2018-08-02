53e7d89753b18d2e8867956e24398ff4227bef9a
mcflugen <mcflugen@gmail.com>
2018-08-02 15:49:50 -0600

    Remove old framework stuff that used nose.

--- bmi_tester/__init__.py
+++ bmi_tester/__init__.py
@@ -1,9 +1,3 @@
-from .bmitester import BmiTester
-from .api import run_test
-
-
-__all__ = ["BmiTester", "run_test"]
-
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
--- bmi_tester/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# Both of these variables should be overriden to test a particular
-# BMI class
-Bmi = None
-INPUT_FILE = None
-BMI_VERSION_STRING = "1.1"
