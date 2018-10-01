3ca8342116d8fb99c47e49bce229d9bd8b83a6f9
mcflugen <mcflugen@gmail.com>
2018-09-30 20:16:10 -0600

    Manifest is either a file name or list of files.

--- bmi_tester/api.py
+++ bmi_tester/api.py
@@ -1,6 +1,9 @@
 import os
+import sys
+
 import pkg_resources
 import pytest
+import six
 
 
 def check_bmi(
@@ -13,12 +16,15 @@ def check_bmi(
 ):
     args = [pkg_resources.resource_filename(__name__, os.path.join("tests_pytest"))]
     os.environ["BMITEST_CLASS"] = package
-    os.environ["BMITEST_INPUT_FILE"] = input_file
+    os.environ["BMITEST_INPUT_FILE"] = input_file or ""
     os.environ["BMI_VERSION_STRING"] = bmi_version
 
-    if manifest is not None:
-        with open(manifest, "r") as fp:
-            manifest = fp.read()
+    if manifest:
+        if isinstance(manifest, six.string_types):
+            with open(manifest, "r") as fp:
+                manifest = fp.read()
+        else:
+            manifest = os.linesep.join(manifest)
         os.environ["BMITEST_MANIFEST"] = manifest
 
     extra_args = extra_args or []
@@ -26,7 +32,5 @@ def check_bmi(
         extra_args.append("--help")
     args += extra_args
 
-    print("Running: {0}".format(" ".join(args)))
+    print("Running: {0}".format(" ".join(args)), file=sys.stderr)
     return pytest.main(args)
-
-
