ba1f4267a2699432f90d7728009881046416e8e5
mcflugen <mcflugen@gmail.com>
2018-09-30 20:40:29 -0600

    Use double quotes.

--- setup.py
+++ setup.py
@@ -4,22 +4,20 @@ from setuptools import setup, find_packages
 import versioneer
 
 
-setup(name='bmi-tester',
-      version=versioneer.get_version(),
-      author='Eric Hutton',
-      author_email='eric.hutton@colorado.edu',
-      description='Test Python BMI bindings.',
-      long_description=open('README.md').read(),
-      packages=find_packages(),
-      cmdclass=versioneer.get_cmdclass(),
-      entry_points={
-          'console_scripts': [
-              # 'bmi-tester=bmi_tester.cmd:main',
-              'bmi-test=bmi_tester.bmipytest:main',
-          ],
-          'bmi.plugins': [
-            'bmi_test=bmi_tester.bmipytest:configure_parser_test',
+setup(
+    name="bmi-tester",
+    version=versioneer.get_version(),
+    author="Eric Hutton",
+    author_email="eric.hutton@colorado.edu",
+    description="Test Python BMI bindings.",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    cmdclass=versioneer.get_cmdclass(),
+    entry_points={
+        "console_scripts": [
+            # 'bmi-tester=bmi_tester.cmd:main',
+            "bmi-test=bmi_tester.bmipytest:main"
         ],
-      }
-
+        "bmi.plugins": ["bmi_test=bmi_tester.bmipytest:configure_parser_test"],
+    },
 )
