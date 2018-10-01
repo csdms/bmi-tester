600a9acfacdda13eb9934502c7d55dd744bd643b
mcflugen <mcflugen@gmail.com>
2018-09-30 20:56:10 -0600

    Fix undefined variables.

--- bmi_tester/tests_pytest/test_grid_unstructured.py
+++ bmi_tester/tests_pytest/test_grid_unstructured.py
@@ -4,9 +4,8 @@ import numpy as np
 def test_grid_x(initialized_bmi, gid):
     gtype = initialized_bmi.get_grid_type(gid)
     if gtype == "unstructured":
-        ndim = bmi.get_grid_rank(gid)
-        size = bmi.get_grid_size(gid)
+        size = initialized_bmi.get_grid_size(gid)
 
         x = np.full(size, np.nan)
-        assert x is bmi.get_grid_x(gid, x)
-        assert np.all(~np.isnan(arr))
+        assert x is initialized_bmi.get_grid_x(gid, x)
+        assert np.all(~np.isnan(x))
