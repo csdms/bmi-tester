b494a5b2ed94c1def6fb8bbbab5df5612ef30aa7
mcflugen <mcflugen@gmail.com>
2018-09-30 21:03:26 -0600

    Test a manifest with multiple files.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,16 @@
+import os
+
 from bmi_tester.api import check_bmi
+
+
+def touch_file(fname):
+    with open(fname, "w"):
+        pass
 
 
 def test_bmi_check(tmpdir):
     with tmpdir.as_cwd():
-        with open("input.yaml", "w"):
-            pass
+        touch_file("input.yaml")
         assert (
             check_bmi(
                 "bmi_tester.bmi:Bmi", input_file="input.yaml", extra_args=["-vvv"]
@@ -15,8 +21,7 @@ def test_bmi_check(tmpdir):
 
 def test_bmi_check_with_manifest_as_list(tmpdir):
     with tmpdir.as_cwd():
-        with open("input.yaml", "w"):
-            pass
+        touch_file("input.yaml")
         assert (
             check_bmi(
                 "bmi_tester.bmi:Bmi",
@@ -31,9 +36,9 @@ def test_bmi_check_with_manifest_as_list(tmpdir):
 def test_bmi_check_with_manifest_as_string(tmpdir):
     with tmpdir.as_cwd():
         with open("manifest.txt", "w") as fp:
-            fp.write("input.yaml")
-        with open("input.yaml", "w"):
-            pass
+            fp.write(os.linesep.join(["input.yaml", "data.dat"]))
+        touch_file("input.yaml")
+        touch_file("data.dat")
         assert (
             check_bmi(
                 "bmi_tester.bmi:Bmi",
