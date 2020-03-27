4b8002da187bb95846e90634d07e54c44c2e613b
mcflugen <mcflugen@gmail.com>
2020-03-27 14:08:32 -0600

    check for metadata using old and new way

--- bmi_tester/api.py
+++ bmi_tester/api.py
@@ -1,7 +1,4 @@
-from __future__ import print_function
-
 import os
-import sys
 
 import pkg_resources
 import pytest
@@ -10,13 +7,17 @@ import six
 
 def check_bmi(
     package,
+    tests_dir=None,
     input_file=None,
     manifest=None,
     bmi_version="1.1",
     extra_args=None,
     help_pytest=False,
 ):
-    args = [pkg_resources.resource_filename(__name__, os.path.join("tests_pytest"))]
+    if tests_dir is None:
+        tests_dir = pkg_resources.resource_filename(__name__, "tests_pytest")
+    args = [tests_dir]
+
     os.environ["BMITEST_CLASS"] = package
     os.environ["BMITEST_INPUT_FILE"] = input_file or ""
     os.environ["BMI_VERSION_STRING"] = bmi_version
@@ -33,10 +34,5 @@ def check_bmi(
     if help_pytest:
         extra_args.append("--help")
     args += extra_args
-
-    print("Running: {0}".format(" ".join(args)), file=sys.stderr)
-    print("export BMITEST_CLASS={0}".format(os.environ["BMITEST_CLASS"]))
-    print("export BMITEST_INPUT_FILE={0}".format(os.environ["BMITEST_INPUT_FILE"]))
-    print("export BMI_VERSION_STRING={0}".format(os.environ["BMI_VERSION_STRING"]))
 
     return pytest.main(args)
