5af7249d4a13c96e6afe6fc8d10ab7048713d769
mcflugen <mcflugen@gmail.com>
2021-03-30 16:57:56 -0600

    import api functions

--- bmi_tester/__init__.py
+++ bmi_tester/__init__.py
@@ -1,4 +1,12 @@
 import pkg_resources
 
+from .api import check_bmi, check_unit_is_valid, check_unit_is_time, check_unit_is_dimensionless
 
 __version__ = pkg_resources.get_distribution("bmi_tester").version
+__all__ = [
+    "check_bmi",
+    "check_unit_is_valid",
+    "check_unit_is_time",
+    "check_unit_is_dimensionless",
+]
+del pkg_resources
