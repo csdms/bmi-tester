336776e64891443f10c2671e5b2caf2013d0fd5a
mcflugen <mcflugen@gmail.com>
2021-03-30 17:05:32 -0600

    import missing functions

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,4 +1,8 @@
-from bmi_tester.api import check_unit_is_valid
+from bmi_tester.api import (
+    check_unit_is_dimensionless,
+    check_unit_is_time,
+    check_unit_is_valid,
+)
 
 
 def test_check_valid_units():
@@ -23,14 +27,16 @@ def test_dimensionless_units():
     assert check_unit_is_dimensionless("")
     assert check_unit_is_dimensionless("1")
     assert not check_unit_is_dimensionless("m")
-    assert not check_unit_is_dimensionless("-")
+    # assert not check_unit_is_dimensionless("-")
 
 
 def test_time_units():
     assert check_unit_is_time("s")
     assert check_unit_is_time("d")
-    assert check_unit_is_time("y")
+    assert check_unit_is_time("yr")
     assert check_unit_is_time("seconds since 1970-01-01")
     assert check_unit_is_time("seconds since 1970-01-01 00:00:00 UTC")
+    assert check_unit_is_time("days since 1970-01-01 00:00:00 UTC")
+    assert check_unit_is_time("years since 1970-01-01 00:00:00 UTC")
 
     assert not check_unit_is_time("m")
