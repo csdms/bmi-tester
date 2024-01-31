5f953965f19b8c25790ab2c2c7e59d8606eb771b
mcflugen <mcflugen@gmail.com>
2024-01-30 23:07:37 -0700

    parametrize some tests

--- tests/test_units.py
+++ tests/test_units.py
@@ -1,40 +1,43 @@
+import pytest
+
 from bmi_tester.api import check_unit_is_dimensionless
 from bmi_tester.api import check_unit_is_time
 from bmi_tester.api import check_unit_is_valid
 
 
-def test_check_valid_units():
-    assert check_unit_is_valid("m")
-    assert check_unit_is_valid("m / s")
-    assert check_unit_is_valid("m s-1")
-    assert check_unit_is_valid("N m")
-    assert check_unit_is_valid("N.m")
-    assert check_unit_is_valid("m^2")
-    assert check_unit_is_valid("m2")
-    assert check_unit_is_valid("")
-    assert check_unit_is_valid("1")
+@pytest.mark.parametrize(
+    "unit", ("m", "m / s", "m s-1", "N m", "N.m", "m^2", "m2", "", "1")
+)
+def test_check_valid_units(unit):
+    assert check_unit_is_valid(unit)
 
 
-def test_check_invalid_units():
-    assert not check_unit_is_valid("foo")
-    assert not check_unit_is_valid("m ** 2")
-    assert not check_unit_is_valid("-")
+@pytest.mark.parametrize("unit", ("foo", "m ** 2", "-"))
+def test_check_invalid_units(unit):
+    assert not check_unit_is_valid(unit)
 
 
-def test_dimensionless_units():
-    assert check_unit_is_dimensionless("")
-    assert check_unit_is_dimensionless("1")
+@pytest.mark.parametrize("unit", ("", "1"))
+def test_dimensionless_units(unit):
+    assert check_unit_is_dimensionless(unit)
+
+
+def test_not_dimensionless_units():
     assert not check_unit_is_dimensionless("m")
-    # assert not check_unit_is_dimensionless("-")
 
 
-def test_time_units():
-    assert check_unit_is_time("s")
-    assert check_unit_is_time("d")
-    assert check_unit_is_time("yr")
-    assert check_unit_is_time("seconds since 1970-01-01")
-    assert check_unit_is_time("seconds since 1970-01-01 00:00:00 UTC")
-    assert check_unit_is_time("days since 1970-01-01 00:00:00 UTC")
-    assert check_unit_is_time("years since 1970-01-01 00:00:00 UTC")
+@pytest.mark.parametrize(
+    "unit",
+    (
+        "s", "d", "yr", "seconds since 1970-01-01",
+        "seconds since 1970-01-01 00:00:00 UTC",
+        "days since 1970-01-01 00:00:00 UTC",
+        "years since 1970-01-01 00:00:00 UTC",
+    )
+)
+def test_time_units(unit):
+    assert check_unit_is_time(unit)
 
+
+def test_not_time_units():
     assert not check_unit_is_time("m")
