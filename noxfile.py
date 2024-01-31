169a881802452654cffb1be7b9b799d5b9c4b7a3
mcflugen <mcflugen@gmail.com>
2024-01-30 23:19:09 -0700

    switch to a src-layout

--- noxfile.py
+++ noxfile.py
@@ -85,7 +85,7 @@ def clean(session):
 
             shutil.rmtree("build", ignore_errors=True)
             shutil.rmtree("dist", ignore_errors=True)
-            shutil.rmtree(f"{PROJECT}.egg-info", ignore_errors=True)
+            shutil.rmtree(f"src/{PROJECT}.egg-info", ignore_errors=True)
             shutil.rmtree(".pytest_cache", ignore_errors=True)
             shutil.rmtree(".venv", ignore_errors=True)
 
